0496267048cec8013198b2899ebd9aef296f8359
Mone19 <fuocoangelos@yahoo.de>
2024-08-31 00:24:47 +0200

    deploy

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B1s"
+    vm_size    = "Standard_D2_v2"
     vnet_subnet_id = azurerm_subnet.subnet.id
   }
 
