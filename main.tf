c8c60228fecac28be6567849ccb07e5e1cbf78db
Mone19 <fuocoangelos@yahoo.de>
2024-08-31 00:00:07 +0200

    hello world

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_B1s"
     vnet_subnet_id = azurerm_subnet.subnet.id
   }
 
